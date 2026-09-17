901bee4ffab8f365b00dbdf26a5b6115916e650c
Aarushi Yadav <aarushi.25b01010854@abes.ac.in>
2026-09-17 16:06:11 +0530

    my fsd workshop

---
+++ backend/Lab-3/httpsserver.js
@@ -0,0 +1,13 @@
+import http from "http";
+import fs from "node:fs/promises";
+
+const server = http.createServer((req, resp) => {
+
+    const url = req.url;
+    const typ = req.method;
+        resp.statusCode = 200;
+        resp.setHeader("Content-Type", "application/json");
+
+        resp.end(JSON.stringify("Hello World"));
+    }
+);
